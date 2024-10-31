26acfc5fc9bc33f87e1956cfe79b7866599d1407
sung-silver <jinna0319@gmail.com>
2024-10-31 18:13:29 +0900

    [FEAT] UserRegisterInfo 구현

---
+++ infra/src/main/java/sopt/makers/authentication/infra/jpa/user/entity/UserRegisterInfo.java
@@ -0,0 +1,28 @@
+package sopt.makers.authentication.infra.jpa.user.entity;
+
+import sopt.makers.authentication.user.*;
+
+import java.time.*;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
+import lombok.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "USER_REGISTER_INFOS")
+public class UserRegisterInfo {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @NotNull private String name;
+  @NotNull private String phone;
+  @NotNull private LocalDate birthday;
+
+  @Min(1)
+  private int generation;
+
+  @NotNull private Part part;
+}
